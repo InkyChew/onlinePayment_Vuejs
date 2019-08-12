e15c3de5c7f44473e473bd6d25cf6742546324c2
InkyChew <B10623019@yuntech.edu.tw>
2019-08-12 11:33:30 +0800

    update

--- src/views/credit.vue
+++ src/views/credit.vue
@@ -38,14 +38,14 @@
           label 有效月年：
           .row.d-flex.flex-row
             .col-3
-              select.form-control
-                option(disable selected) 月
+              select(v-model="formData.month").form-control
+                option(disabled :value="''") 月
                 template(v-for="m in 12")
                   option {{ m }}
             label.align-self-end /
             .col-3
-              select.form-control.col
-                option(disable selected) 年
+              select(v-model="formData.year").form-control.col
+                option(disabled :value="''") 年
                 template(v-for="y in 10")
                   option {{ 2019 + y - 1}}
         .form-group
@@ -60,8 +60,8 @@
             label 付款人信箱：
             input.form-control(type='email', ref='email', v-model='formData.email')
         .form-check
-          input.form-check-input(type='checkbox')
-          label.form-check-label 請再次確認「訂單資訊」與「付款資訊」，付款完成後將方送通知信至您的E-mail信箱
+          input.form-check-input(type='checkbox' v-model='formData.checked')
+          label.form-check-label.red 請再次確認「訂單資訊」與「付款資訊」，付款完成後將方送通知信至您的E-mail信箱
           label.form-text.text-muted 第三方支付金流平台服務條款
 
     .button.d-flex.justify-content-between
@@ -69,11 +69,11 @@
         :to="'/'"
       )
         button.btn.btn-primary(type='button') 回上一步
-      router-link(
-        :to="'/finish'"
-      )
-        button.btn.btn-primary(type='button') 確認付款
-        //- button.btn.btn-primary(type='button' @click='validate') 確認付款
+      //- router-link(
+      //-   :to="'/finish'"
+      //- )
+        //- button.btn.btn-primary(type='button') 確認付款
+      button.btn.btn-primary(type='button' @click='validate') 確認付款
 </template>
 
 <script>
@@ -88,7 +88,10 @@ export default {
         c2: '',
         c3: '',
         c4: '',
-        back3: ''
+        back3: '',
+        month: '',
+        year: '',
+        checked: false
       }
     }
   },
@@ -106,22 +109,31 @@ export default {
         nextTarget.focus()
       }
     },
-    validate () {
-      var all = document.getElementsByClassName('form-control');
+    validate () { // 送出表單前驗證
+      const all = document.getElementsByClassName('form-control')
       console.log(all)
-      for (var i = 0; i < all.length; i++) {
-        all[i].style.borderColor = 'red';
-      }
-      const { email, c1, c2, c3, c4, back3 } = this.formData
-      
-      if ( email === '' || c1 === '' || c2 === '' || c3 === '' || c4 === '' || back3 === '') {
-        alert("欄位不得為空白，請確認後再送出，謝謝!")
+      const { email, c1, c2, c3, c4, back3, month, year, checked } = this.formData
+
+      if (email === '' || c1 === '' || c2 === '' || c3 === '' || c4 === '' || back3 === '') {
+        for (var i = 0; i < all.length; i++) {
+          all[i].style.borderColor = 'red'
+        }
+        alert('欄位不得為空白，請確認後再送出，謝謝!')
       } else {
-        const emailReg = / ^ ([\w\.\-]){1,64} \@ ([\w\.\-]){1,64} $ /
-        if (emailReg.test(this.formData.email)) {
-          alert("email格式不正確，請確認後再送出，謝謝!")
+        const emailReg = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/
+        if (!emailReg.test(this.formData.email)) {
+          all[7].style.borderColor = 'red'
+          alert('email格式不正確，請確認後再送出，謝謝!')
+        } else if (month === '' || year === '') {
+          all[4].style.borderColor = 'red'
+          all[5].style.borderColor = 'red'
+          alert('請選擇有效月/年後再送出，謝謝!')
+        } else if (checked === false) {
+          const red = document.getElementsByClassName('red')
+          red[0].style.color = 'red'
+          alert('請再次確認「訂單資訊」與「付款資訊」，並勾選欄位後再送出，謝謝!')
         } else {
-          this.router.push({ path: 'finish' })
+          this.$router.push({ path: 'finish' })
         }
       }
     }
