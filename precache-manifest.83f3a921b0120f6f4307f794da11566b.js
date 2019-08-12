203650143bd1fc412ce4af766e0932deb2a09ba5
InkyChew <B10623019@yuntech.edu.tw>
2019-08-12 09:01:43 +0800

    Updates

---
+++ precache-manifest.83f3a921b0120f6f4307f794da11566b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1162909878d73836f69641b79bbfb63",
+    "url": "/img/web-atm.c1162909.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e6bbdc837778264da15a145c35299320",
+    "url": "/img/mastercard.e6bbdc83.svg"
+  },
+  {
+    "revision": "f0a466fd2e284ef08710",
+    "url": "/css/app.e6b46bc4.css"
+  },
+  {
+    "revision": "3397756d38e4ac53bf5a",
+    "url": "/js/chunk-vendors.72a6771c.js"
+  },
+  {
+    "revision": "2e8c17ac7fd2e48a613367ba06a59ac0",
+    "url": "/img/atm.2e8c17ac.svg"
+  },
+  {
+    "revision": "32d419e67c7f5cc6af9f32478ec866ef",
+    "url": "/img/unionpay.32d419e6.svg"
+  },
+  {
+    "revision": "9eac96abeb701a8069ac538496ecf9db",
+    "url": "/img/visa.9eac96ab.svg"
+  },
+  {
+    "revision": "5ba15b35480ce255bee76580c1bbe878",
+    "url": "/img/jcb.5ba15b35.svg"
+  },
+  {
+    "revision": "3628ef65e0118da97a64",
+    "url": "/js/about.0a42b97e.js"
+  },
+  {
+    "revision": "f0a466fd2e284ef08710",
+    "url": "/js/app.f34a6a91.js"
+  },
+  {
+    "revision": "4d888f64be49bf0e0d31ec50c4409e33",
+    "url": "/img/credit-card.4d888f64.svg"
+  },
+  {
+    "revision": "674e1db015b3f9d016df4ffcb79b67da",
+    "url": "/img/shop.674e1db0.svg"
+  },
+  {
+    "revision": "4119e1248cd21f639e771898358751dc",
+    "url": "/img/finish.4119e124.svg"
+  },
+  {
+    "revision": "2a9ec68bc2131bde63990f2f7e87b35c",
+    "url": "/img/back-three.2a9ec68b.svg"
+  },
+  {
+    "revision": "576d29f06b4d757b7b594968071cc0a5",
+    "url": "/img/footer-background.576d29f0.svg"
+  },
+  {
+    "revision": "965ccfcec389715e0bcf824de781b669",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3628ef65e0118da97a64",
+    "url": "/css/about.86a58cba.css"
+  }
+];
